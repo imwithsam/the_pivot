066941e743c0e780df5ab80c8a3ab4d1e41da56d
Jason Wright <noobjey@gmail.com>
2015-08-27 11:51:03 -0600

    refactored to use payment processor

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -5,65 +5,56 @@ class ChargesController < ApplicationController
 
     # Amount in cents
     @amount        = total_in_cents.to_i
+    @order         = create_order
+    add_events_to_order(@order.id, cart)
+    empty_cart
 
-    charge = pay(customer)
-
-    if charge["paid"] == true
-      @order = Order.create(user_id: current_user.id,
-                            status:  "paid")
-
-      cart.cart_items.each do |cart_item|
-        EventOrder.create(order_id:   @order.id,
-                          event_id:   cart_item.id,
-                          quantity:   cart_item.quantity,
-                          unit_price: cart_item.price)
-      end
-
-      session[:cart] = {}
-      cart.clear
-
-      flash[:success] = "Your payment was successful and your order is placed."
-      notify_boss
-      redirect_to order_path(@order)
-    end
-
-
-    private
-
-    def notify_boss
-      client = Twilio::REST::Client.new(ENV["twilio_account_sid"],
-                                        ENV["twilio_auth_token"])
-      client.messages.create(from: "5005550006",
-                             to:   "3039002304",
-                             body: "You've received a $#{(@order.total)} order!")
-    end
-
-    def pay
-      make_payment
-
-    rescue Stripe::CardError => e
-      payment_failed_process(e)
-    end
-  end
-
-  def make_payment
-    Stripe::Charge.create(
-      customer:    stripe_customer_id,
-      amount:      @amount,
-      description: Order.last.id + 1,
-      currency:    'usd'
+    payment_processor = PaymentProcessor.new(
+      current_user.email,
+      params[:stripeToken],
+      @amount
     )
+
+    if payment_processor.make_payment
+      notify_boss
+      flash[:success] = "Your payment was successful and your order is placed."
+      redirect_to order_path(@order)
+    else
+      flash[:error] = "There was a problem processing your payment."
+      redirect_to cart_path
+    end
+
+
   end
 
-  def payment_failed_process(e)
-    flash[:error] = e.message
-    redirect_to cart_path
+  def add_events_to_order(id, cart)
+    cart.cart_items.each do |cart_item|
+      EventOrder.create(order_id:   id,
+                        event_id:   cart_item.id,
+                        quantity:   cart_item.quantity,
+                        unit_price: cart_item.price)
+    end
   end
 
-  def stripe_customer_id
-    Stripe::Customer.create(
-      email: current_user.email,
-      card:  params[:stripeToken]
-    ).id
+
+  private
+
+  def create_order
+    Order.create(user_id: current_user.id,
+                 status:  "ordered")
   end
+
+  def notify_boss
+    client = Twilio::REST::Client.new(ENV["twilio_account_sid"],
+                                      ENV["twilio_auth_token"])
+    client.messages.create(from: "5005550006",
+                           to:   "3039002304",
+                           body: "You've received a $#{(@order.total)} order!")
+  end
+
+  def empty_cart
+    session[:cart] = {}
+    cart.clear
+  end
+
 end
