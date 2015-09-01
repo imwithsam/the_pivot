d1f99c665ca29f1add1b27f4e28e8a20b2e4597b
Jason Wright <noobjey@gmail.com>
2015-09-01 16:38:58 -0600

    refactore controller and tests

--- spec/features/users_can_login_spec.rb
+++ spec/features/users_can_login_spec.rb
@@ -3,12 +3,7 @@ require "factory_helper"
 
 feature "a user can login" do
   scenario "an existing user logs in" do
-    User.create(first_name: "Jane",
-                last_name: "Doe",
-                username: "Jane's Shop",
-                email: "jane@doe.com",
-                password: "12345678")
-
+    build_test_data
     visit root_path
     click_link "Login"
 
@@ -18,20 +13,20 @@ feature "a user can login" do
     expect(page).to have_content("Password")
     expect(page).to have_button("Login")
 
-    fill_in "Email", with: "jane@doe.com"
-    fill_in "Password", with: "12345678"
+    fill_in "Email", with: @user_1.email
+    fill_in "Password", with: "password"
     click_button "Login"
 
     expect(current_path).to eq(dashboard_path)
     within(".alert-success") do
-      expect(page).to have_content("Welcome back to The Ocho Tickets, Jane Doe!")
+      expect(page).to have_content("Welcome back to The Ocho Tickets, #{@user_1.first_name} #{@user_1.last_name}!")
     end
     within(".navbar-right") do
-      expect(page).to have_link("Logged in as Jane Doe")
+      expect(page).to have_link("Logged in as #{@user_1.first_name} #{@user_1.last_name}")
     end
-    expect(page).to have_content("Jane")
-    expect(page).to have_content("Doe")
-    expect(page).to have_content("jane@doe.com")
+    expect(page).to have_content(@user_1.first_name)
+    expect(page).to have_content(@user_1.last_name)
+    expect(page).to have_content(@user_1.email)
     expect(page).to_not have_content("Login")
     expect(page).to have_content("Logout")
   end
@@ -47,7 +42,7 @@ feature "a user can login" do
     expect(page).to have_button("Login")
 
     fill_in "Email", with: "jane@doe.com"
-    fill_in "Password", with: "12345678"
+    fill_in "Password", with: "password"
     click_button "Login"
 
     expect(current_path).to eq(login_path)
@@ -59,7 +54,7 @@ feature "a user can login" do
       expect(page).to_not have_content("Logged in as Jane Doe")
     end
     expect(page).to_not have_content("Jane")
-    expect(page).to_not have_content("Doe")
+    expect(page).to_not have_content("Dow")
     expect(page).to_not have_content("jane@doe.com")
     expect(page).to have_content("Create Account")
     expect(page).to have_content("Login")
