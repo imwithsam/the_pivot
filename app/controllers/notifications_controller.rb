bf123ddc776205678e2ad7e82531da58191d6768
Samson Brock <SamsonLB@gmail.com>
2015-09-01 15:08:34 -0600

    Initial commit for base ActionMailer functionality

---
+++ app/controllers/notifications_controller.rb
@@ -0,0 +1,13 @@
+class NotificationsController < ApplicationController
+  def create
+    NotificationsMailer.contact(email_params).deliver_now
+
+    redirect_to  :back, notice: "Your email has been sent."
+  end
+
+  private
+
+  def email_params
+    params.permit(:name, :email, :message)
+  end
+end
