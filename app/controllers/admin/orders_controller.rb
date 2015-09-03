629bc37535479e73205a3e5de533d77beb4e6e54
dsshim <daveshim@gmail.com>
2015-09-03 11:30:00 -0600

    adds admin controller refactoring

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,45 +1,51 @@
 class Admin::OrdersController < Admin::BaseController
+  before_action :find_all_orders, only: [:index, :index_ordered, :index_paid,
+                                         :index_cancelled, :index_completed]
+  before_action :find_single_order, only: [:show, :update]
+
   def index
-    @orders = Order.all
     @status = :all
   end
 
   def index_ordered
-    @orders = Order.all
     @status = :ordered
     render :index
   end
 
   def index_paid
-    @orders = Order.all
     @status = :paid
     render :index
   end
 
   def index_cancelled
-    @orders = Order.all
     @status = :cancelled
     render :index
   end
 
   def index_completed
-    @orders = Order.all
     @status = :completed
     render :index
   end
 
   def show
-    @order = Order.find(params[:id])
-
     capability = Twilio::Util::Capability.new ENV["twilio_account_sid"], ENV["twilio_auth_token"]
     capability.allow_client_outgoing ENV["twilio_twiml_app_sid"]
     @token = capability.generate
   end
 
   def update
-    order = Order.find(params[:id])
-    order.status = params[:status]
-    order.save
+    @order.status = params[:status]
+    @order.save
     redirect_to admin_orders_path
+  end
+
+  private
+
+  def find_all_orders
+    @orders = Order.all
+  end
+
+  def find_single_order
+    @order = Order.find(params[:id])
   end
 end
